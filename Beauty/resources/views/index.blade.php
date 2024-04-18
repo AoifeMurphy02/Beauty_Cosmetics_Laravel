47345de14a052436ca4201c29544f6789ef30f3d
meghana1974 <145559822+meghana1974@users.noreply.github.com>
2024-04-18 13:23:57 +0100

    Create index.blade.php for home page

---
+++ Beauty/resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Laravel 8 CRUD Tutorial From Scratch</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+</head>
+<body>
+<p> Hello!</p>
+</body>
+</html>
