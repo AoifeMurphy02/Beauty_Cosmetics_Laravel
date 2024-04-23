489f7a0818db0d2de2839ff59172e78ec3ac7726
AoifeMurphy02 <aoifemurphy2019@gmail.com>
2024-04-23 12:12:14 +0100

    Adder header and footer in layouts

--- Beauty/resources/views/index.blade.php
+++ Beauty/resources/views/index.blade.php
@@ -1,11 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Laravel 8 CRUD Tutorial From Scratch</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
-</head>
-<body>
-<p> Hello!</p>
-</body>
-</html>
+@extends('layouts.app')
+
+@section('content')
+    <p>Hello!</p>
+@endsection
---
+++ Beauty/resources/views/layouts/app.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>Nails and Beauty</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+</head>
+<body>
+    <header>
+        <p>header!</p>
+    </header>
+    <div>
+        @yield('content')
+    </div>
+    <footer>
+        @include('layouts.footer')
+    </footer>
+</body>
+</html>
