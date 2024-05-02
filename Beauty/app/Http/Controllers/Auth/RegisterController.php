36e8005a1a868e11d244199a7e23b1873c360266
meghana1974 <145559822+meghana1974@users.noreply.github.com>
2024-05-02 11:50:25 +0100

    Create RegisterController to handle the form submission

---
+++ Beauty/app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+
+class RegisterController extends Controller
+{
+    public function showRegistrationForm()
+    {
+        return view('auth.register');
+    }
+
+    public function register(Request $request)
+{
+    // Validate the request data
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users',
+        'password' => 'required|string|min:6|confirmed', 
+    ]);
+
+    // Create the user
+    $user = User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => Hash::make($request->password), 
+    ]);
+
+ 
+    Auth::login($user);
+
+
+    return redirect()->route('index'); 
+}
+
+    
+}
