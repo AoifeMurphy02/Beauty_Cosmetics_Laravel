e139f04d20633015fa14d799b349d0ec58f39b9c
meghana1974 <145559822+meghana1974@users.noreply.github.com>
2024-05-07 14:44:04 +0100

    Update app.blade.php.

--- Beauty/resources/views/layouts/app.blade.php
+++ Beauty/resources/views/layouts/app.blade.php
@@ -1,35 +1,62 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <link rel="icon" type="image/ico" href="{{ asset('images/favicon.ico') }}">
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Nails and Beauty</title>
-    <script src="{{ asset('js/app.js') }}" defer></script>
 
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
+    <!-- Styles -->
+    <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-    <header>
-        <a class="headerbanner" href="#">
-            <img src="../../public/css/images/nailbar.png"/>
-          </a>
-          <nav>
-            <a  href="/">Home</a>
-            <a  href="/services" >Services</a>
-            <a  href="/appointments" >Appointments</a>
-            <a href="/staff" >Staff</a>
-            <a  href="/aboutUs" >About Us</a>
-            <a href="/gallery" >Gallery</a>
-            <a href="/register" >Register</a>
-    </nav>
-    </header>
-    <div>
+    <div id="app">
+        <header>
+            <div class="container mx-auto flex justify-between items-center px-6">
+                <div>
+                    <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
+                        {{ config('app.name', 'Laravel') }}
+                    </a>
+                </div>
+                <nav>
+                    <a class="headerbanner" href="#">
+                        <img src="{{ asset('css/images/nailbar.png') }}"/>
+                    </a>
+                    <a href="/">Home</a>
+                    <a href="/services">Services</a>
+                    <a href="/appointments">Appointments</a>
+                    <a href="/staff">Staff</a>
+                    <a href="/aboutUs">About Us</a>
+                    <a href="/gallery">Gallery</a>
+                </nav>
+                <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
+                    @guest
+                        <a class="no-underline hover:underline" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        @if (Route::has('register'))
+                            <a class="no-underline hover:underline" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        @endif
+                    @else
+                        <span>{{ Auth::user()->name }}</span>
+
+                        <a href="{{ route('logout') }}"
+                           class="no-underline hover:underline"
+                           onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="hidden">
+                            {{ csrf_field() }}
+                        </form>
+                    @endguest
+                </nav>
+            </div>
+        </header>
+
         @yield('content')
     </div>
-    <footer>
-        @include('layouts.footer')
-    </footer>
 </body>
 </html>
