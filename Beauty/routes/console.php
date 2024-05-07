3071b5b87c814e2c38b4c1baf63f922f83b3bc24
meghana1974 <145559822+meghana1974@users.noreply.github.com>
2024-05-07 14:42:58 +0100

    Update welcome.blade.php and console.php

--- Beauty/routes/console.php
+++ Beauty/routes/console.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Artisan::command('example', function () {
+    $this->info('This command does something.');
+})->describe('Description of what the command does');
+
