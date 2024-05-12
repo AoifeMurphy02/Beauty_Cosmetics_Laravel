cfbb18ef6837a870656b999372873fe1880a2098
Meghank1066 <D00254682@student.dkit.ie>
2024-05-12 12:14:27 +0100

    footer blade

--- Beauty/resources/views/layouts/footer.blade.php
+++ Beauty/resources/views/layouts/footer.blade.php
@@ -1,6 +1,28 @@
-{{-- <footer>   
-    <p>
-        Copyright 2024. All Rights Reserved
-    </p>
-
-</footer> --}}
+<footer>
+    <div class="footer-columns">
+      <div class="footer-column">
+        <h3>About Nails</h3>
+        <ul>
+          <li><a href="#">Our Story</a></li>
+          <li><a href="#">Services</a></li>
+          <li><a href="#">Contact Us</a></li>
+        </ul>
+      </div>
+      <div class="footer-column">
+        <h3>Developers</h3>
+        <ul>
+          <li><a href="#">Meghan</a></li>
+          <li><a href="#">Meghana</a></li>
+          <li><a href="#">Aoife</a></li>
+        </ul>
+      </div>
+      <div class="footer-column">
+        <h3>Follow Us</h3>
+        <ul>
+          <li><a href="#">Facebook</a></li>
+          <li><a href="#">Instagram</a></li>
+          <li><a href="#">Twitter</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
